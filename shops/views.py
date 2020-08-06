46d3d83e19453fcc193f73f0b3994b233478a051
Byeonguk Yoo <bwyoo1229@gmail.com>
2020-08-06 14:57:24 +0900

    shops views detail, give_rating function add

--- shops/views.py
+++ shops/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
+
 from shops.models import *
 
 
@@ -6,3 +8,17 @@ def index(request):
     shops = Shop.objects.all()
     return render(request, 'shops/index.html', {'shops': shops})
 
+
+def detail(request, pk):
+    shop = get_object_or_404(Shop, pk=pk)
+    return render(request, 'shops/detail.html', {'shop': shop})
+
+
+def give_rating(request, pk):
+    shop = get_object_or_404(Shop, pk=pk)
+    if request.method == 'POST':
+        shop.rating = request.POST['rating']
+        shop.save()
+        return redirect(reverse('shops:give_rating', kwargs={'pk': pk}))
+
+    return render(request, 'shops/give_rating.html', {'shop': shop})
