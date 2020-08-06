485d9f9677e0c61f231a03e409a9e25ab1c514b3
Byeonguk Yoo <bwyoo1229@gmail.com>
2020-08-06 13:26:37 +0900

    shops model file update

--- cars/admin.py
+++ cars/admin.py
@@ -8,3 +8,11 @@ class CarAdmin(admin.ModelAdmin):
     """ Car Admin Definition """
 
     pass
+
+
+@admin.register(models.Brand)
+class BrandAdmin(admin.ModelAdmin):
+
+    """ Brand Admin Definition"""
+
+    pass
--- shops/models.py
+++ shops/models.py
@@ -8,23 +8,20 @@ class Rating(core_models.TimeStampedModel):
 
     """ Rating Model Definition """
 
-    star = models.PositiveSmallIntegerField()
-
-    def __str__(self):
-        return self.star
+    pass
 
 
 class Shop(core_models.TimeStampedModel):
 
     """ Shop Model Definition """
 
-    name = models.CharField(max_length=140)
-    description = models.TextField()
-    region = models.CharField(max_length=80)
-    city = models.CharField(max_length=80)
-    address = models.CharField(max_length=80)
-    homepage = models.URLField(max_length=250)
-    phone_number = models.IntegerField()
+    name = models.CharField(max_length=140, blank=True)
+    description = models.TextField(blank=True)
+    region = models.CharField(max_length=80, blank=True)
+    city = models.CharField(max_length=80, blank=True)
+    address = models.CharField(max_length=80, blank=True)
+    homepage = models.URLField(max_length=250, blank=True)
+    phone_number = models.IntegerField(blank=True)
 
     def __str__(self):
         return self.name
@@ -35,5 +32,5 @@ class Favorite(core_models.TimeStampedModel):
     """ Favorite Model Definition"""
 
     user = models.ManyToManyField(User, related_name='favorites')
-    shop = models.ForeignKey(Shop)
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
     total = models.IntegerField(default=0)
