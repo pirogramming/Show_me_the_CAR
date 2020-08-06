528c6d04e95565e0355bfc9d99d02a28b2576a35
Byeonguk Yoo <bwyoo1229@gmail.com>
2020-08-06 16:45:34 +0900

    샵 별로 평점 구분 하고 주석 완료

--- shops/views.py
+++ shops/views.py
@@ -10,14 +10,21 @@ def index(request):
 
 
 def give_rating(request, pk):
+    # POST 방식
+    # 나의 평점을 POST 방식으로 추가하면 rating query에 평점이 등록됨
     if request.method == 'POST':
         rating = get_object_or_404(Rating, pk=pk)
         rating.Rating = request.POST['rating']
         rating.save()
         return redirect(reverse('shops:give_rating', kwargs={'pk': pk}))
 
+    # GET 방식
     shop = get_object_or_404(Shop, pk=pk)
-    ratings = Rating.objects.values('rating')
+
+    # 특정 샵의 Pk로 먼저 필터링 후 그 샵의 rating field 값만 불러와서 리스트 생성
+    ratings = Rating.objects.filter(shop=pk).values('rating')
+
+    # 생성한 ratings 딕셔너리에서 value 값만 추출해서 평균 구하는 루프
     rating_sum = 0
     for r in ratings:
         r_value = list(r.values())
@@ -27,3 +34,4 @@ def give_rating(request, pk):
     return render(request, 'shops/give_rating.html', {
         'shop': shop, 'average': average
     })
+
