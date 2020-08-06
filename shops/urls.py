fc0902fc97dde429c08b35cfa811aeae7e45a111
Byeonguk Yoo <bwyoo1229@gmail.com>
2020-08-06 15:00:45 +0900

    shops urls detial, give_rating add

--- shops/urls.py
+++ shops/urls.py
@@ -8,4 +8,6 @@ app_name = 'shops'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', index, name='index'),
+    path('detail/<int:pk>', detail, name='detail'),
+    path('detail/<int:pk>/give_rating/', give_rating, name='give_rating'),
 ]
