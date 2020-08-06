eb7c9f96e0267111d0ed74b488bc4cc287c935b1
Byeonguk Yoo <bwyoo1229@gmail.com>
2020-08-06 16:00:09 +0900

    shops/views rating system complete

--- shops/views.py
+++ shops/views.py
@@ -10,6 +10,12 @@ def index(request):
 
 
 def give_rating(request, pk):
+    if request.method == 'POST':
+        rating = get_object_or_404(Rating, pk=pk)
+        rating.Rating = request.POST['rating']
+        rating.save()
+        return redirect(reverse('shops:give_rating', kwargs={'pk': pk}))
+
     shop = get_object_or_404(Shop, pk=pk)
     ratings = Rating.objects.values('rating')
     rating_sum = 0
@@ -17,12 +23,6 @@ def give_rating(request, pk):
         r_value = list(r.values())
         rating_sum = rating_sum + sum(r_value)
     average = rating_sum/len(ratings)
-
-    if request.method == 'POST':
-        rating = get_object_or_404(Rating, pk=pk)
-        rating.Rating = request.POST['rating']
-        rating.save()
-        return redirect(reverse('shops:give_rating', kwargs={'pk': pk}))
 
     return render(request, 'shops/give_rating.html', {
         'shop': shop, 'average': average
