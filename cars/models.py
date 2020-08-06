fd84342f981d5093240007d4492fff82459520be
Woo-yeong Kim <kimwy1997@gmail.com>
2020-08-06 11:05:21 +0900

    Create Brand Model

--- cars/models.py
+++ cars/models.py
@@ -2,13 +2,24 @@ from django.db import models
 from core import models as core_models
 
 
+class Brand(core_models.TimeStampedModel):
+
+    """ Brand Model Definition """
+
+    name = models.CharField(max_length=80)
+    homepage = models.URLField(max_length=240)
+
+    def __str__(self):
+        return self.name
+
+
 class Car(core_models.TimeStampedModel):
 
     """ Car Model Definition """
 
     model_name = models.CharField(max_length=80)
-    brand = models.CharField(max_length=80)
     color = models.CharField(max_length=80)
-    homepage = models.URLField(max_length=250)
+    url = models.URLField(max_length=250)
+    brand = models.ForeignKey("Brand", on_delete=models.SET_NULL, related_name="cars")
     shop = models.ManyToManyField("shops.Shop", related_name="cars", blank=True)
 
