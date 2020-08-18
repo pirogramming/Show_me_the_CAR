8484fb3fb97bc1ce1071b09ea10d921e48072b76
Woo-yeong Kim <kimwy1997@gmail.com>
2020-08-18 20:58:45 +0900

    Add createsu command

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from core import managers as core_managers
 
 
 class User(AbstractUser):
@@ -9,3 +10,4 @@ class User(AbstractUser):
     avatar = models.ImageField(blank=True)
     birthdate = models.DateField(blank=True, null=True)
     superhost = models.BooleanField(default=False)
+    objects = core_managers.CustomUserManager()
