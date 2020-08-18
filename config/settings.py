499ea68e3a84d6a9e65920759764e25800e1165a
Woo-yeong Kim <kimwy1997@gmail.com>
2020-08-18 20:19:50 +0900

    Set DB NAME in settings.py obvious

--- config/settings.py
+++ config/settings.py
@@ -108,7 +108,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "HOST": os.environ.get("RDS_HOST"),
-        "NAME": os.environ.get("RDS_NAME"),
+        "NAME": "postgres",
         "USER": os.environ.get("RDS_USER"),
         "PASSWORD": os.environ.get("RDS_PASSWORD"),
         "PORT": "5432",
