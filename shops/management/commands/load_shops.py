74ce29199381a15d03fad18bbd8f79e70307fe31
Woo-yeong Kim <kimwy1997@gmail.com>
2020-08-20 22:26:36 +0900

    Change csv file encoding to utf-8

--- shops/management/commands/load_shops.py
+++ shops/management/commands/load_shops.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         path = kwargs["path"]
-        with open(path, "rt") as f:
+        with open(path, "rt", encoding="utf-8") as f:
             reader = csv.reader(f, dialect={"encoding": "utf-8-sig"})
             orders = list(reader)
             for row in orders:
